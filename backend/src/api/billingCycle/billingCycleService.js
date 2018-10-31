d9c750a6bb1429ab9c660cd81ce1c0a38324036a
Gustavo Weber <gustavo.weber@rcadigital.com.br>
2018-10-31 18:54:30 -0200

    update libraries react

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -16,7 +16,7 @@ BillingCycle.route('count', (req, res, next) => {
 })
 
 BillingCycle.route('summary', (req, res, next) => {
-    BillingCycle.aggregate({
+    BillingCycle.aggregate([{
         $project: {
             credit: {
                 $sum: "$credits.value"
@@ -37,7 +37,7 @@ BillingCycle.route('summary', (req, res, next) => {
             credit: 1,
             debt: 1
         }
-    }, (error, result) => {
+    }]).exec((error, result) => {
         if(error) {
             res.status(500).json({errors: [error]})
         } else {
---
+++ frontend-old/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Gustavo Weber &copy; 2018
+            <a href="https://github.com/gustavoweber" target="_blank"> GitHub</a>
+        </strong>
+    </footer>
+)
